2ea262abe0eb94e5edd595d517174fd7a58550a0
GHuyHuynh <huy.huynh@dal.ca>
2024-10-05 16:25:14 -0300

    [Fix] button z-index

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,17 +1,16 @@
-import { useState } from 'react'
 import { CSky } from './components/CSky'
 import { HomeBackground } from './layout/home-background'
 
 function App() {
 
   return (
-    <main className='flex flex-1 flex-col h-screen w-screen'>
-      {/* <Sky /> */}
-      {/* <CSky /> */} 
-      <div className='flex-1'>
-        <HomeBackground />
-      </div>
+    <main className='w-screen h-screen'>
+
+      <HomeBackground />
+
     </main>
+
+    
       
   )
 }
