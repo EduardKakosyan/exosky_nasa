c43a8b06120a62312c9159b9ee86f2253228c24e
Eduard Kakosyan <90533005+EduardKakosyan@users.noreply.github.com>
2024-10-05 12:10:21 -0300

    added main for the celestial pipeline

---
+++ back-end/pipeline/celestial/main.py
@@ -0,0 +1,14 @@
+""" Main Function to run the celestial pipeline
+
+@author: Eduard Kakosyan
+"""
+
+import logging
+
+
+def main():
+    logging.basicConfig(level=logging.INFO)
+
+
+if __name__ == "__main__":
+    main()
