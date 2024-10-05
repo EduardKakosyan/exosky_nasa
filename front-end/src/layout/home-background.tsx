7cf0283108731baca45be83a5dfd79b353976145
GHuyHuynh <huy.huynh@dal.ca>
2024-10-05 16:25:14 -0300

    [Fix] button z-index

--- front-end/src/layout/home-background.tsx
+++ front-end/src/layout/home-background.tsx
@@ -1,11 +1,29 @@
 import { ShootingStars } from "../components/ui/shooting-stars";
 import { StarsBackground } from "../components/ui/stars-background";
+import { MagicButton } from "../components/magic-button";
+
 export function HomeBackground() {
+   function onExploreClick() {
+      console.log("Explore button clicked");
+   }
+
+
    return (
       <div className="h-full bg-black flex flex-col items-center justify-center relative w-full">
          <h2 className="relative flex-col md:flex-row z-10 text-3xl md:text-5xl md:leading-tight max-w-5xl mx-auto text-center tracking-tight font-medium bg-clip-text text-transparent bg-gradient-to-b from-neutral-800 via-white to-white flex items-center gap-2 md:gap-8 h-full">
             <span className="text-6xl">Exosky</span>
          </h2>
+
+         <div className="mb-32">
+            <MagicButton onClickCall={onExploreClick}/>
+         </div>
+
+         
+
+
+
+
+
          <ShootingStars />
          <StarsBackground />
       </div>
