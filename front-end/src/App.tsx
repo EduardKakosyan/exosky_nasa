e73996b71cfbbe5a7c1ad0e6e7f05a6346287854
GHuyHuynh <huy.huynh@dal.ca>
2024-10-05 15:52:46 -0300

    [Add] stars background

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,14 +1,17 @@
 import { useState } from 'react'
 import { CSky } from './components/CSky'
+import { HomeBackground } from './layout/home-background'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
+    <main className='flex flex-1 flex-col h-screen w-screen'>
       {/* <Sky /> */}
-      <CSky />
-    </>
+      {/* <CSky /> */} 
+      <div className='flex-1'>
+        <HomeBackground />
+      </div>
+    </main>
       
   )
 }
