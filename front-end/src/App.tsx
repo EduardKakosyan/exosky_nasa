3484d3b2fc81f8c50b9371c21bcff5eed58414a3
GHuyHuynh <huy.huynh@dal.ca>
2024-10-05 11:31:13 -0300

    [Add] front-end dependencies

---
+++ front-end/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='bg-blue-200'>Hello World</h1>
+    </>
+      
+  )
+}
+
+export default App
