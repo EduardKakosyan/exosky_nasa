bfaaf4fde6ec94a0ee015fb094ce07e399049813
GHuyHuynh <huy.huynh@dal.ca>
2024-10-06 14:36:39 -0300

    [done]

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -8,9 +8,12 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/earth" element={<CSky imgSrc='sky_image.jpg'/>} />
+        <Route path="/earth" element={<CSky imgSrc='sky_image.png'/>} />
         <Route path="/55" element={<CSky imgSrc='55_Cancri_e.png'/>} />
         <Route path="/wasp" element={<CSky imgSrc='WASP-12b.png'/>} />
+        <Route path="/trappist" element={<CSky imgSrc='TRAPPIST-1e.png'/>} />
+        <Route path="/kepler" element={<CSky imgSrc='Kepler-186f.png'/>} />
+        <Route path='/hd' element={<CSky imgSrc='hd.png'/>} />
       </Routes>
     </Router>
   );
