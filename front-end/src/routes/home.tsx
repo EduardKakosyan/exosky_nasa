28e3b0b4541caa44a374aea5cf98e68e45ea5d9a
GHuyHuynh <huy.huynh@dal.ca>
2024-10-12 21:03:59 -0300

    [Fix] compatibility issues

--- front-end/src/routes/home.tsx
+++ front-end/src/routes/home.tsx
@@ -11,32 +11,32 @@ export function Home() {
    const [exploreMode, setExploreMode] = useState(false);
 
    return (
-      <div className="h-screen bg-black flex flex-col items-center justify-center relative w-screen">
+      <div className="min-h-screen bg-black flex flex-col items-center justify-center relative w-screen">
          {!exploreMode && (
-             <>
-                 <h2 className="relative flex-col md:flex-row z-10 text-3xl md:text-5xl md:leading-tight max-w-5xl mx-auto text-center tracking-tight font-medium bg-clip-text text-transparent bg-gradient-to-b from-neutral-800 via-white to-white flex items-center gap-2 md:gap-8 h-full">
-                     <span className="text-6xl">Sky Painters</span>
-                 </h2>
+            <div className="w-screen h-screen flex flex-col items-center my-32 gap-32">
+               <h2 className="relative flex-col md:flex-row z-10 text-3xl md:text-5xl md:leading-tight max-w-5xl mx-auto text-center tracking-tight font-medium bg-clip-text text-transparent bg-gradient-to-b from-neutral-800 via-white to-white flex items-center gap-2 md:gap-8">
+                  <span className="text-6xl">Sky Painters</span>
+               </h2>
 
-                 <h3 className="relative md:flex-row z-10 text-3xl md:text-5xl md:leading-tight max-w-5xl mx-auto text-center tracking-tight font-medium bg-clip-text text-transparent bg-gradient-to-b from-neutral-800 via-white to-white flex items-center gap-2 md:gap-8 h-full">
-                     <span className="text-4xl">NASA Space Apps Challenge 2024</span>
-                 </h3>
+               <h3 className="relative md:flex-row z-10 text-3xl md:text-5xl md:leading-tight max-w-5xl mx-auto text-center tracking-tight font-medium bg-clip-text text-transparent bg-gradient-to-b from-neutral-800 via-white to-white flex items-center gap-2 md:gap-8">
+                  <span className="text-4xl">NASA Space Apps Challenge 2024</span>
+               </h3>
 
-                 <div className="mb-32">
-                     <ExploreButton onExploreCall={() => setExploreMode(true)}/>
-                 </div>
-             </>
+               <div className="mb-32">
+                  <ExploreButton onExploreCall={() => setExploreMode(true)}/>
+               </div>
+            </div>
          )};
 
 
           {exploreMode && (
-              <div className="w-screen h-screen flex flex-col mt-20">
+              <div className="w-screen h-screen flex flex-col mt-24">
                   <div className="absolute top-4 left-4 z-50">
                       <HomeButton onHomeCall={() => setExploreMode(false)}/>
                   </div>
 
                   <div className="flex justify-center items-center w-full h-full">
-                  <div className="grid 2xl:grid-cols-2 xl:grid-cols-2 lg:grid-cols-2  md:grid-cols-1 sm:grid-cols-1 gap-2 h-full mx-auto">
+                  <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-1 xl:grid-cols-2 gap-5 h-full mx-auto">
                      {planetArray.map((item, index) => (
                      <div key={index} className="z-10 mx-10">
                         <PlanetCard planetObject={item}/>
@@ -49,12 +49,11 @@ export function Home() {
          )};
 
          
-
-
-
-
-         <ShootingStars />
-         <StarsBackground />
+         <div className="w-screen min-h-screen">
+            <ShootingStars />
+            <StarsBackground />
+         </div>
+         
       </div>
    );
 }
